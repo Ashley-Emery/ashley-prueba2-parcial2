b1bbde491271a41322e3b36aef201bb92de77e1d
Ashley Fajardo <a.e.fajardo06@gmail.com>
2026-06-04 14:58:51 -0600

    initial commit

---
+++ src/ashley/prueba2/parcial2/AshleyPrueba2Parcial2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ashley.prueba2.parcial2;
+
+/**
+ *
+ * @author ashley
+ */
+public class AshleyPrueba2Parcial2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
